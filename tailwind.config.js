ec23539598239e3619c23a33ce98eab332867010
vydlknh <dolinhkhanhvy2004@gmail.com>
2023-08-11 20:54:04 +0700

    change header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./components/*astro'],
   theme: {
     extend: {},
   },
   plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'forest-green': '#034c3c',
+      },
+    }
+  }
 }
 
