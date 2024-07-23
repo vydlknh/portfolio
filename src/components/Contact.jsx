e97c3b5804b479d5db329a46b2fc343f0162883e
Vy <vykl.do04@gmail.com>
2024-07-23 11:35:52 +0700

    added responsiveness

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,40 @@
 function Contact() {
 
   return (
-    <>
-      <div className="container is-flex is-justify-content-center mt-5">
-        <p className="buttons">
-          <a className="button" href="https://www.linkedin.com/in/vydlknh/">
-            <span className="icon">
-              <i class="fa-brands fa-linkedin"></i>
-            </span>
-            <span>LinkedIn</span>
-          </a>
-          <a className="button" href="https://github.com/vydlknh">  
-            <span class="icon">
-              <i class="fab fa-github"></i>
-            </span>
-            <span>GitHub</span>
-          </a>
-          <a className="button" href="https://drive.google.com/file/d/1BX7vgQq2zXR1X74hrsAw8PxFsSuss9K_/view?usp=sharing">
-            <span className="icon">
-              <i class="fa-solid fa-file"></i>
-            </span>
-            <span>Resume</span>
-          </a>
-        </p>
+    <section id="contact">
+      <div className="columns is-mobile">
+        <div className="column"></div>
+        <div className="column pt-6">
+          <div className="level is-mobile">
+              <div className="level-item">
+                <a className="button" href="https://www.linkedin.com/in/vydlknh/">
+                  <span className="icon">
+                    <i class="fa-brands fa-linkedin"></i>
+                  </span>
+                  <span>LinkedIn</span>
+                </a>
+              </div>
+              <div className="level-item">
+                <a className="button" href="https://github.com/vydlknh">  
+                  <span class="icon">
+                    <i class="fab fa-github"></i>
+                  </span>
+                  <span>GitHub</span>
+                </a>
+              </div>
+              <div className="level-item">
+                <a className="button" href="https://drive.google.com/file/d/1BX7vgQq2zXR1X74hrsAw8PxFsSuss9K_/view?usp=sharing">
+                  <span className="icon">
+                    <i class="fa-solid fa-file"></i>
+                  </span>
+                  <span>Resume</span>
+                </a>
+              </div>
+          </div>
+        </div>
+        <div className="column"></div>
       </div>
-    </>
+    </section>
   )
   
 }
